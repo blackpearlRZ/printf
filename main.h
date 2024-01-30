8d66f8029cca161e12b0261aa51be64a900a4a86
chaimae <chaimaeaafif@gmail.com>
2024-01-30 14:37:04 -0800

    header

--- main.h
+++ main.h
@@ -2,48 +2,23 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdarg.h>
-#include <limits.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
-
-
-/**
- * struct format - match the conversion specifiers for printf
- * @id: type char pointer of the specifier i.e (l, h) for (d, i, u, o, x, X)
- * @f: type pointer to function for the conversion specifier
- *
- */
-
-typedef struct format
-{
-	char *id;
-	int (*f)();
-} convert_match;
-
-int printf_pointer(va_list val);
-int printf_hex_aux(unsigned long int num);
-int printf_HEX_aux(unsigned int num);
-int printf_exclusive_string(va_list val);
-int printf_HEX(va_list val);
-int printf_hex(va_list val);
-int printf_oct(va_list val);
-int printf_unsigned(va_list args);
-int printf_bin(va_list val);
-int printf_srev(va_list args);
-int printf_rot13(va_list args);
-int printf_int(va_list args);
-int printf_dec(va_list args);
-int _strlen(char *s);
-int *_strcpy(char *dest, char *src);
-int _strlenc(const char *s);
-int rev_string(char *s);
-int _strlenc(const char *s);
-int printf_37(void);
-int printf_char(va_list val);
-int printf_string(va_list val);
-int _putchar(char c);
+int _char(va_list type);
+int _str(va_list type);
+int _str_num(char *s);
+int _mod(va_list type);
 int _printf(const char *format, ...);
+int _number(int n, char spec);
+int _print_number(unsigned long int n, char spec, int sign);
+int _print_binary(unsigned int n);
+int _nonprintable(char *str);
+int pointer_print(void *pointer);
+int reverse_print(char *s);
+int _rot13(char *s);
+
 
 #endif
